44c48dda62497652dd01e9a11b64f718608c2898
Tomasz Buszewski <tomek.buszewski@gmail.com>
2024-08-17 20:47:07 +0200

    feat: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,17 +4,28 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import react from "eslint-plugin-react";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    settings: { react: { version: "18.3" } },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "simple-import-sort": simpleImportSort,
@@ -45,6 +56,8 @@ export default tseslint.config(
         },
       ],
       "simple-import-sort/exports": "error",
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
     },
   },
 );
