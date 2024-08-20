50f923ecc7a72e26bf04626ecdd5483a463a035b
Tomasz Buszewski <tomek.buszewski@gmail.com>
2024-08-20 16:18:27 +0200

    feat: libraries offloading

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import { cdn } from "vite-plugin-cdn2";
+import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { configDefaults } from "vitest/config";
 
@@ -13,7 +15,24 @@ export default defineConfig(({ mode }) => {
   }
 
   return {
-    plugins: [tsconfigPaths(), react()],
+    plugins: [
+      tsconfigPaths(),
+      react(),
+      checker({ typescript: true }),
+      cdn({
+        modules: [
+          {
+            name: "react",
+            relativeModule: "./umd/react.production.min.js",
+          },
+          {
+            name: "react-dom",
+            relativeModule: "./umd/react-dom.production.min.js",
+            aliases: ["client"],
+          },
+        ],
+      }),
+    ],
     preview: {
       port: Number(port),
       strictPort: true,
